89395f0b3c236e1031574a0e99121867f8e33b2a
Nova <64618129+x1325990526@users.noreply.github.com>
2026-02-14 18:51:23 +0800

    Add getComputerChoice() to random  get rock | paper | scissors

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
- console.log("Hello World")
+ console.log(getComputerChoice());
+ function getComputerChoice(){
+    let n = Math.random();
+    if(n<0.33) return "rock";
+    if(n>=0.33 && n<0.66) return "paper";
+    if(n>=0.66) return "scissors";
+ }
