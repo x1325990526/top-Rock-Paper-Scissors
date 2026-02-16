0ee024eb3b4eebde8bd446cb1083b593dce7e103
Nova <64618129+x1325990526@users.noreply.github.com>
2026-02-16 10:41:15 +0800

    Add playRound

--- script.js
+++ script.js
@@ -1,15 +1,46 @@
- function getComputerChoice(){
+function getComputerChoice(){
     const choices = ["rock","paper","scissors"];
     //Math.floor 向下取整
     //Math.random * choices.length 返回 0-2.999...之间的数
     return choices[Math.floor(Math.random() * choices.length)];
  }
 
-console.log(getComputerChoice());
 
 function getHumanChoice(){
 
     return prompt("input rock or paper or scissors","rock");
 }
 
-console.log(getHumanChoice());
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+  humanChoice.toLowerCase();
+  if (humanChoice != "rock" && humanChoice != "scissors" && humanChoice != "paper"){
+    console.log("Please input rock or paper or scissors");
+    return 1;
+  } 
+
+  if(humanChoice === "rock" && computerChoice === "scissors" || humanChoice === "scissors" && computerChoice === "paper" || humanChoice === "paper" && computerChoice === "rock"){
+
+    humanScore++;
+    console.log(`You win! ${humanChoice} beats ${computerChoice}`);
+
+  }else if(humanChoice === computerChoice){
+
+     console.log(`It's a tie! ${humanChoice} tied ${computerChoice}`);
+     
+  }else{
+
+    computerScore++;
+     console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+
+  }
+
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
